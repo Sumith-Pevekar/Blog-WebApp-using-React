8d47588e6ca61439c1e709483c8b0c7a4ec7e712
sumithp <sumithpevekar20@gmail.com>
2022-12-29 11:30:21 -0800

    Added Create.js

---
+++ src/Home.js
@@ -0,0 +1,18 @@
+import useFetch from "./useFetch";
+import BlogList from "./BlogList";
+const Home = () => {
+  const {
+    data: blogs,
+    isPending,
+    error,
+  } = useFetch("http://localhost:8000/blogs");
+  return (
+    <div className="home">
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
+    </div>
+  );
+};
+
+export default Home;
