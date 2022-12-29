6033d51e4d29d71adcf5a1505eb5e4e7eaea6eeb
sumithp <sumithpevekar20@gmail.com>
2022-12-29 11:49:29 -0800

    Added AbortController

--- src/useFetch.js
+++ src/useFetch.js
@@ -5,8 +5,9 @@ const useFetch = (url) => {
   const [isPending, setIsPending] = useState(true);
   const [error, setError] = useState(null);
   useEffect(() => {
+    const abortCont = new AbortController();
     setTimeout(() => {
-      fetch(url)
+      fetch(url, { signal: abortCont.signal })
         .then((res) => {
           if (!res.ok) {
             throw Error("could not fetch the data for that resource");
@@ -19,10 +20,15 @@ const useFetch = (url) => {
           setError(null);
         })
         .catch((err) => {
-          setIsPending(false);
-          setError(err.message);
+          if (err.name === "AbortError") {
+            console.log("fetch aborted");
+          } else {
+            setIsPending(false);
+            setError(err.message);
+          }
         });
     }, 1000);
+    return () => abortCont.abort();
   }, [url]);
   return { data, isPending, error };
 };
