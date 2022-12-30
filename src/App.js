ce9ffdbcaccfc64f7c6f2be9128db3b51da6ac6f
sumithp <sumithpevekar20@gmail.com>
2022-12-30 00:05:45 -0800

    Added blog details

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from "./Navbar";
 import Home from "./Home";
 import Create from "./Create";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import BlogDetails from "./BlogDetails";
 function App() {
   return (
     <Router>
@@ -14,6 +15,9 @@ function App() {
             </Route>
             <Route path="/create">
               <Create />
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogDetails />
             </Route>
           </Switch>
         </div>
