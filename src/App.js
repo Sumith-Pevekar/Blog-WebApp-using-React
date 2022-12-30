238c893899866c27d35c31bb21cda67c6b6b9bf7
sumithp <sumithpevekar20@gmail.com>
2022-12-30 01:03:18 -0800

    Added 404 redirect

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./Home";
 import Create from "./Create";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import BlogDetails from "./BlogDetails";
+import NotFound from "./NotFound";
 function App() {
   return (
     <Router>
@@ -18,6 +19,9 @@ function App() {
             </Route>
             <Route path="/blogs/:id">
               <BlogDetails />
+            </Route>
+            <Route path="*">
+              <NotFound />
             </Route>
           </Switch>
         </div>
